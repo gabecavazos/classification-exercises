0dd132d086ff335bcb6a87e1ad7a7955fae03853
Gabe Cavazos <gabecavazos3@gmail.com>
2023-03-13 15:47:09 -0500

    evaluation

--- acquire.py
+++ acquire.py
@@ -24,23 +24,12 @@ def new_titanic_data():
 
 def get_titanic_data():
     '''
-    This function reads in titanic data from Codeup database, writes data to
-    a csv file if a local file does not exist, and returns a df.
+    This function reads in titanic data from Codeup database.
     '''
-    if os.path.isfile('titanic_df.csv'):
-        
-        # If csv file exists, read in data from csv file.
-        df = pd.read_csv('titanic_df.csv', index_col=0)
-        
-    else:
-        
-        # Read fresh data from db into a DataFrame.
-        df = new_titanic_data()
-        
-        # Write DataFrame to a csv file.
-        df.to_csv('titanic_df.csv')
-        
-    return df
+        my_query = "SELECT * FROM passengers"
+        df = pd.read_sql(my_query, get_connection('titanic_db'))
+        return df
+
 
 def new_iris_data():
     '''
